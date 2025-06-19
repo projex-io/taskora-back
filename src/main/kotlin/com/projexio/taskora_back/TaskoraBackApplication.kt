5bdc07ac2fc7967b441bc215c557c90e87f8ce3f
aassalax <aassali.dev@gmail.com>
2025-06-19 19:38:14 +0200

    first commit

---
+++ src/main/kotlin/com/projexio/taskora_back/TaskoraBackApplication.kt
@@ -0,0 +1,11 @@
+package com.projexio.taskora_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskoraBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaskoraBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/projexio/taskora_back/TaskoraBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.projexio.taskora_back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskoraBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
