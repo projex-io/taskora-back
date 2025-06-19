f04af0eadbd072e612b121ad4d4d21ce15b66f64
aassalax <aassali.dev@gmail.com>
2025-06-19 21:43:54 +0200

    add a schema to help testcontainers

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tickets (
+                         id SERIAL PRIMARY KEY,
+                         title VARCHAR(255),
+                         description TEXT,
+                         status VARCHAR(50)
+);
