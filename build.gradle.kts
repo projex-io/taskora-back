dbb56342366a894dbc07d14785aaeb849571a166
aassalax <aassali.dev@gmail.com>
2025-06-19 21:31:40 +0200

    add testcontainers

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.postgresql:postgresql:42.7.7")
 
+	testImplementation("org.testcontainers:testcontainers:1.19.7")
+	testImplementation("org.testcontainers:junit-jupiter:1.19.7")
+	testImplementation("org.testcontainers:postgresql:1.19.7")
 	testImplementation("io.mockk:mockk:1.13.9")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
