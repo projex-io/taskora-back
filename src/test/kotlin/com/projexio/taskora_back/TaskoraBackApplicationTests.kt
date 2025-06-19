8f4bf2051e653ae2efb4c91a6fe48dc0aa27a5f1
Antoine Assali <128133517+aassalax@users.noreply.github.com>
2025-06-19 21:45:57 +0200

    workflow - ci (#2)
    * workflow - ci
    
    * update CI workflow to use upload-artifact v4
    
    * update CI to use JDK 21 and disable failing test
    
    * add testcontainers
    
    * testcontainers setup
    
    * add a schema to help testcontainers

--- src/test/kotlin/com/projexio/taskora_back/TaskoraBackApplicationTests.kt
+++ src/test/kotlin/com/projexio/taskora_back/TaskoraBackApplicationTests.kt
@@ -2,12 +2,35 @@ package com.projexio.taskora_back
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 @SpringBootTest
 class TaskoraBackApplicationTests {
 
 	@Test
 	fun contextLoads() {
+	}
+
+	companion object {
+		@Container
+		val postgres = PostgreSQLContainer("postgres:15-alpine").apply {
+			withDatabaseName("testdb")
+			withUsername("testuser")
+			withPassword("testpass")
+		}
+
+		@JvmStatic
+		@DynamicPropertySource
+		fun overrideProps(registry: DynamicPropertyRegistry) {
+			registry.add("spring.datasource.url", postgres::getJdbcUrl)
+			registry.add("spring.datasource.username", postgres::getUsername)
+			registry.add("spring.datasource.password", postgres::getPassword)
+		}
 	}
 
 }
